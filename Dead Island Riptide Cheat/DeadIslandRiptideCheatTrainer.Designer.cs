f472f1ecf172aadcc725eedf35bf3e5e318f4877
marce <marce@DESKTOP-QA28VU9>
2021-11-20 10:12:22 +0100

    Projektdateien hinzufügen.

---
+++ Dead Island Riptide Cheat/DeadIslandRiptideCheatTrainer.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace Dead_Island_Riptide_Cheat
+{
+    partial class DeadIslandRiptideCheatTrainer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DeadIslandRiptideCheatTrainer));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(45)))), ((int)(((byte)(73)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(15)))), ((int)(((byte)(83)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(35)))), ((int)(((byte)(73)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(127, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Max Rank";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_2);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(45)))), ((int)(((byte)(73)))));
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(15)))), ((int)(((byte)(83)))));
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(35)))), ((int)(((byte)(73)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(127, 153);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 39);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Max Geld";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.FlatAppearance.BorderSize = 0;
+            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.checkBox1.Location = new System.Drawing.Point(402, 114);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(130, 37);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "God Mode";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.FlatAppearance.BorderSize = 0;
+            this.checkBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox2.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.checkBox2.Location = new System.Drawing.Point(402, 157);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(237, 37);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Unendlich   Munition";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(94, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(45)))), ((int)(((byte)(73)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(632, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 35);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(45)))), ((int)(((byte)(73)))));
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.BlueViolet;
+            this.label1.Location = new System.Drawing.Point(138, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(468, 47);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Dead Island Riptide DFE Trainer";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.FlatAppearance.BorderSize = 0;
+            this.checkBox3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox3.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.checkBox3.Location = new System.Drawing.Point(402, 200);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(229, 37);
+            this.checkBox3.TabIndex = 9;
+            this.checkBox3.Text = "Waffen   Haltbarkeit";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.FlatAppearance.BorderSize = 0;
+            this.checkBox4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox4.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.checkBox4.Location = new System.Drawing.Point(127, 200);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(252, 37);
+            this.checkBox4.TabIndex = 10;
+            this.checkBox4.Text = "Unbegrenzte Ausdauer";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // DeadIslandRiptideCheatTrainer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(45)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(672, 263);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "DeadIslandRiptideCheatTrainer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DeadIsland Riptide Cheat Trainer";
+            this.Load += new System.EventHandler(this.DeadIslandRiptideCheatTrainer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+    }
+}
+
---
+++ Dead Island Riptide Cheat/DeadIslandRiptideCheatTrainer.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+using System.Runtime.InteropServices;
+
+namespace Dead_Island_Riptide_Cheat
+{
+    public partial class DeadIslandRiptideCheatTrainer : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+            (
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+            );
+
+
+        public Point mouseLocation;
+
+
+
+        Mem mMemLib = new Mem();
+        public DeadIslandRiptideCheatTrainer()
+        {
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+        }
+
+        private void DeadIslandRiptideCheatTrainer_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            mMemLib.OpenProcess("DeadIslandRiptideGame");
+            Thread TH = new Thread(WriteMemory);
+            Thread FT = new Thread(WriteMemory2);
+            Thread HK = new Thread(WriteMemory3);
+            Thread UA = new Thread(WriteMemory4);
+
+            TH.Start();
+            FT.Start();
+            HK.Start();
+            UA.Start();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        private void button2_Click_2(object sender, EventArgs e)
+        {
+            if (button2.Enabled)
+            {
+                mMemLib.WriteMemory("gamedll_x64_rwdi.dll+0x01281C08,20,30,20,80", "int", "24396500");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Enabled)
+            {
+                mMemLib.WriteMemory("gamedll_x64_rwdi.dll+0x012822C8,D20,180,BC8", "int", "999999999");
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void WriteMemory()
+        {
+            while (true)
+            {
+                if (checkBox1.Checked)
+                {
+                    mMemLib.WriteMemory("gamedll_x64_rwdi.dll+0x012822F8,628,48,20,D04", "int", "2131741184");
+                }
+
+                Thread.Sleep(1);
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void WriteMemory2()
+        {
+            while (true)
+            {
+                if (checkBox2.Checked)
+                {
+                    mMemLib.WriteMemory("gamedll_x64_rwdi.dll+0x012822F8,158,88,50", "int", "1337");
+                }
+
+                Thread.Sleep(1);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Enabled)
+            {
+                Close();
+            }
+        }
+
+
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseLocation = new Point(-e.X, -e.Y);
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mousePose = Control.MousePosition;
+                mousePose.Offset(mouseLocation.X, mouseLocation.Y);
+                Location = mousePose;
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WriteMemory3()
+        {
+
+            /*weitere pointer offsets für haltbarkeit
+             * 1. "gamedll_x64_rwdi.dll+0x0128BA58,208,9B8,18,8,54"
+             * 2. "gamedll_x64_rwdi.dll+0x0128BA58,208,40,0,978,54"
+             * 3. "gamedll_x64_rwdi.dll+0x0128BA58,208,9B8,20,978,54"
+             * 
+             * Standart haltbarkeit upgrade 4 BBQ waffe 1119944730
+             */
+            while (true)
+            {
+                if (checkBox3.Checked)
+                {
+                    mMemLib.WriteMemory("gamedll_x64_rwdi.dll+0x0128BA58,208,9B8,18,8,54", "int", "2119944730");
+                }
+
+                Thread.Sleep(1);
+            }
+        }
+
+        private void WriteMemory4()
+        {
+
+            /*
+             * weitere pointer offsets für die Ausdauer
+             * 1. "engine_x64_rwdi.dll+0x00A51F58,280,128,30,20,D94"
+             * 2. "engine_x64_rwdi.dll+0x00A6F7C8,510,128,30,20,D94"
+             * 3. "engine_x64_rwdi.dll+0x00A6F7D0,510,128,30,20,D94"
+             * 4. "gamedll_x64_rwdi.dll+0x01281860,48,38,268,0,D5C"
+             * 5. "gamedll_x64_rwdi.dll+0x012824D0,8,3B8,C0,60,D5C"
+             * 6. "gamedll_x64_rwdi.dll+0x012824C0,10,3B8,C0,60,D5C"
+             * 
+             * Standart Ausdauer wert ohne upgrades 1065353216
+             */
+            while (true)
+            {
+                if (checkBox4.Checked)
+                {
+                    mMemLib.WriteMemory("engine_x64_rwdi.dll+0x00A51F58,280,128,30,20,D94", "int", "1065353216");
+                }
+
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
